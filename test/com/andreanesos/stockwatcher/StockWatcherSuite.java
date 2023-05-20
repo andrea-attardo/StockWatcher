0339e1fd177ecc0d0d95563fa62b97e31a4a279a
Andrea Attardo <105414820+andrea-attardo@users.noreply.github.com>
2023-05-20 18:17:26 +0200

    ui done

---
+++ test/com/andreanesos/stockwatcher/StockWatcherSuite.java
@@ -0,0 +1,14 @@
+package com.andreanesos.stockwatcher;
+
+import com.andreanesos.stockwatcher.client.StockWatcherTest;
+import com.google.gwt.junit.tools.GWTTestSuite;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class StockWatcherSuite extends GWTTestSuite {
+  public static Test suite() {
+    TestSuite suite = new TestSuite("Tests for StockWatcher");
+    suite.addTestSuite(StockWatcherTest.class);
+    return suite;
+  }
+}
